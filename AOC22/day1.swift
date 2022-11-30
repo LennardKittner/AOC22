3f8585fb95a459f463c166ccc115c90a34cfda85
Lennard Kittner <lennardkittner@icloud.com>
2022-11-30 22:12:35 +0100

    initial commit

---
+++ AOC22/day1.swift
@@ -0,0 +1,11 @@
+//
+//  day1.swift
+//  AOC22
+//
+//  Created by Lennard on 30.11.22.
+//
+
+import Foundation
+
+func day1(fileContent: String) {
+}
---
+++ AOC22/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  AOC22
+//
+//  Created by Lennard on 30.11.22.
+//
+
+import Foundation
+
+if CommandLine.argc < 3 {
+    print("To few arguemtns.")
+    exit(1)
+}
+
+let day = Int(CommandLine.arguments[1]) ?? 0
+let WD = CommandLine.arguments[2]
+
+let path = WD + "day\(day).txt"
+
+if let fileContent = try? String(contentsOfFile: path) {
+    switch day {
+        case 1: day1(fileContent: fileContent)
+        default:
+            print("Day not found.")
+    }
+}
+
